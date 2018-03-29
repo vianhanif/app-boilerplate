52152ac315cf0a2e75cd6407ad22b44b2cd43da6
Alvian Rahman Hanif <alvian524@gmail.com>
2018-03-29 13:36:36 +0700

    asds

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { hydrate, render } from 'react-dom'
 import { Provider } from 'react-redux'
 import './assets/scss/index.scss'
 import Home from './pages/Home'
 import store, { history } from './store'
 import { Route } from 'react-router'
 import { ConnectedRouter } from 'react-router-redux'
-import registerServiceWorker from './registerServiceWorker'
+// import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(
+const rootElement = document.getElementById('root')
+[rootElement.hasChildNodes() ? 'hydrate' : 'render'](
   <Provider store={store}>
     <ConnectedRouter history={history}>
-      <Route exact path="/" component={Home}/>
+      <Route exact path="/" component={Home}></Route>
     </ConnectedRouter>
   </Provider>,
-  document.getElementById('root')
+  rootElement
 )
-registerServiceWorker()
+// registerServiceWorker()
