e8f56a528a8ed7468dfbdf6b3c2c51f1c0ab945e
Alvian Rahman Hanif <alvian524@gmail.com>
2018-04-02 11:11:14 +0700

    update packages

--- src/store/app/actions.js
+++ src/store/app/actions.js
@@ -3,14 +3,18 @@ import * as types from './types'
 export default {
   [types.LOAD_DATA] (commit, {data, callback}) {
     // assume there an API being hit in here
-    let payload = {
-      ...data,
-      result: 'data for 0'
-    }
-    commit({
-      type: types.SETUP,
-      payload
-    })
-    callback()
+    commit({type: types.SETUP, payload: 'Loading...'})
+    let payload = null
+    setTimeout(() => {
+      payload = {
+        ...data,
+        result: 'data for 0'
+      }
+      commit({
+        type: types.SETUP,
+        payload
+      })
+      callback()
+    }, 1000)
   }
 }
